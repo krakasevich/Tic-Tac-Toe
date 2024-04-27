d9133ad39e893c87e46203e4b842df739560129a
Aleksandr Krakasevich <skrakasevic1@gmail.com>
2024-04-27 16:29:48 +0200

    Implement symbol switching for players

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,38 +1,45 @@
 class Game():
     
-    def __init__(self, symbol):
+    def __init__(self, player1, player2):
         self.board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-        self.symbol = symbol
-        # self.player2 = 'o'
+        self.player1 = player1
+        self.player2 = player2
+        self.current_player = 1
 
     def print_board(self):
-        print('+----+----+----+')
+        print('+---+---+---+')
         for row in range(3):
             row_str = '|'
             for col in range(3):
-                row_str += '  ' + self.board[row][col] + ' |'
+                row_str += ' ' + self.board[row][col] + ' |'
             print(row_str)
-            print('+----+----+----+')
+            print('+---+---+---+')
+
+    def switch_player(self):
+        if self.current_player == 1:
+            self.current_player = 2
+        else:
+            self.current_player = 1
 
     def move(self):
-        row = int(input('Enter the row number (0, 1, 2): '))
-        col = int(input('Enter the col number (0, 1, 2): '))
+        while True:
+            row = int(input('Enter the row number (0, 1, 2): '))
+            col = int(input('Enter the col number (0, 1, 2): '))
 
-        # проверка ввода
-
-        if 0 <= row <= 2 and 0 <= col <= 2:
-            if self.board[row][col] == ' ':
-                if self.symbol == 'x':
-                    self.symbol == 'o'
-                else:
-                    self.symbol == 'x'
-                self.board[row][col] == self.symbol
+            if 0 <= row <= 2 and 0 <= col <= 2:
+                if self.board[row][col] == ' ':
+                    if self.current_player == 1:
+                        self.board[row][col] = self.player1
+                    else:
+                        self.board[row][col] = self.player2
+                    self.switch_player()
+                    self.print_board()
             else:
                 print('This cell is already occupied. Please choose another one')
-        self.print_board()
+        
 
 if __name__ == '__main__':
-    x = Game('x')
+    x = Game('x', 'o')
   
     x.print_board()
     x.move()

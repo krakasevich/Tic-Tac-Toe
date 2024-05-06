8e9cf0f73128553636596e25edd2c62dab715ce0
Aleksandr Krakasevich <skrakasevic1@gmail.com>
2024-05-07 00:06:44 +0200

    Title: Fully implemented Tic-Tac-Toe game with  bugs to fix
    Body: Implemented the full functionality of the Tic-Tac-Toe game, including board setup, player turns, win condition checking, and tie detection. However, there are a few minor bugs that need to be addressed:
    
    - The display of the winning player is incorrect

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,9 +1,8 @@
 class Game():
-    
-    def __init__(self, player1, player2):
+
+    def __init__(self, player):
         self.board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-        self.player1 = player1
-        self.player2 = player2
+        self.player = player
         self.current_player = 1
 
     def print_board(self):
@@ -17,32 +16,79 @@ class Game():
 
     def switch_player(self):
         if self.current_player == 1:
+            self.player = 'o'
             self.current_player = 2
         else:
             self.current_player = 1
+            self.player = 'x'
 
     def move(self):
-        while True:
-            row = int(input('Enter the row number (0, 1, 2): '))
-            col = int(input('Enter the col number (0, 1, 2): '))
+        if self.current_player == 1:
+            row = int(input('Player {} enter the row number (0, 1, 2): '.format(self.current_player)))
+            col = int(input('Player {} enter the col number (0, 1, 2): '.format(self.current_player)))
+        else:
+            row = int(input('Player {} enter the row number (0, 1, 2): '.format(self.current_player)))
+            col = int(input('Player {} enter the col number (0, 1, 2): '.format(self.current_player)))
 
-            if 0 <= row <= 2 and 0 <= col <= 2:
-                if self.board[row][col] == ' ':
-                    if self.current_player == 1:
-                        self.board[row][col] = self.player1
-                    else:
-                        self.board[row][col] = self.player2
-                    self.switch_player()
-                    self.print_board()
+        while row not in range(3) or col not in range(3):
+            print('Error, please enter a number between 0 and 2')
+            row = int(input('Player {} enter the row number (0, 1, 2): '.format(self.current_player)))
+            col = int(input('Player {} enter the col number (0, 1, 2): '.format(self.current_player)))
+            
+        if 0 <= row <= 2 and 0 <= col <= 2:
+            if self.board[row][col] == ' ':
+                if self.current_player == 1:
+                    self.board[row][col] = self.player
+                else:
+                    self.board[row][col] = self.player
+                self.switch_player()
             else:
                 print('This cell is already occupied. Please choose another one')
-        
 
+    def check_win(self):
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] == 'x' or self.board[i][0] == self.board[i][1] == self.board[i][2] == 'o':
+                return True
+            elif self.board[0][i] == self.board[1][i] == self.board[2][i] == 'x' or self.board[0][i] == self.board[1][i] == self.board[2][i] == 'o':
+                return True
+            elif self.board[0][0] == self.board[1][1] == self.board[2][2] == 'x' or self.board[0][0] == self.board[1][1] == self.board[2][2] == 'o':
+                return True
+            elif self.board[0][2] == self.board[1][1] == self.board[2][0] == 'x' or self.board[0][2] == self.board[1][1] == self.board[2][0] == 'o':
+                return True
+        return False
+    
+    def won_game(self):
+        return self.check_win() 
+
+def game():
+    print('---------------------------RULES---------------------------\n'
+        'The game is played on a grid that\'s 3 squares by 3 squares. \n'
+        'You are "x" , your friend is "o" . Players take turns putting their marks in empty squares. \n'
+        'The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.\n'
+        'When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.\n'
+        '-------------------------GOOD LUCK-------------------------')
+    while True:    
+        n = 9
+        x = Game('x')
+        while n > 0:
+            n -= 1
+            x.print_board()
+            x.move()
+            if x.won_game() == True:
+                x.print_board()
+                print('Player {} wins. Congratulations!!!'.format(x.current_player))
+                break
+            elif n == 1:
+                x.print_board()
+                print('It\'s a tie. :(')
+                break
+
+        play_again = input('Do you want to play again? (yes/no): ')
+        if play_again.lower() not in ['yes', 'y', 'ye']:
+            break
+        
 if __name__ == '__main__':
-    x = Game('x', 'o')
-  
-    x.print_board()
-    x.move()
+    game()
     
     
     
